5c8d44a61d2dc63e51e7490f2f63966c9c4ace71
ingridadine <66760636+ingridadine@users.noreply.github.com>
2025-01-09 10:09:01 -0300

    Create a conditional render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,15 @@ import './App.css'
 import MovieCard from './components/MovieCard'
 
 function App() {
+  const movieNumber = 1;
+  
   return (
     <>
-      <MovieCard movie={{title: "Dini's Film", release_date: "2024"}}/>
-      <MovieCard movie={{title: "Roverdi's Film", release_date: "2020"}}/>
+      {movieNumber === 2 ? (
+        <MovieCard movie={{title: "Dini's Film", release_date: "2024"}}/>
+      ) : (
+        <MovieCard movie={{title: "Roverdi's Film", release_date: "2020"}}/>
+      )}
     </>
   )
 }
