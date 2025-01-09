ebaa71d25d93b278206aa9598ff9de41f0057c30
ingridadine <66760636+ingridadine@users.noreply.github.com>
2025-01-08 23:59:06 -0300

    Create first component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Text />
+    </>
+  )
+}
+
+function Text() {
+  return (
+    <div>
+      <p>Hello World! 🎉</p>
+    </div>
+  )
+}
+
+export default App
